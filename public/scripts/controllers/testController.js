32da8933e2ff8675501038f0f111072349c42604
Brian Schorn <schorn.brian@gmail.com>
2017-03-03 13:24:26 -0600

    Can add reservations to DB now from TestPage

--- public/scripts/controllers/testController.js
+++ public/scripts/controllers/testController.js
@@ -44,10 +44,12 @@ _this.testBike2 = {
 //Bike Buttons linking to BikeService
   _this.getBikes = function(){
     bikeService.getBikes().then(function(bikeList){
-      _this.bikeList = bikeList;
+      _this.bikeList = bikeList.data;
       console.log(_this.bikeList);
     });
   };
+  _this.getBikes();
+
   _this.addBike = function(bike){
     bikeService.addBike(bike).then(function(bikeList){
       _this.bikeList = bikeList;
@@ -100,7 +102,7 @@ _this.testBike2 = {
     });
   };
 
-  //
+  //Calendar Stuff
   _this.today = function() {
     console.log('today')
     _this.dt = {};
@@ -115,7 +117,7 @@ _this.clear = function() {
   _this.dt = null;
 };
 
-$scope.submitDates = function(dt) {
+_this.submitDates = function(dt) {
   console.log('Dates:', dt)
   // check dates
   _this.clear();
@@ -161,9 +163,7 @@ $scope.open2 = function() {
   $scope.popup2.opened = true;
 };
 
-$scope.setDate = function(year, month, day) {
-  $scope.dt = new Date(year, month, day);
-};
+
 
 $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
 $scope.format = $scope.formats[0];
@@ -177,20 +177,20 @@ $scope.popup2 = {
   opened: false
 };
 
-var tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1);
-var afterTomorrow = new Date();
-afterTomorrow.setDate(tomorrow.getDate() + 1);
-$scope.events = [
-  {
-    date: tomorrow,
-    status: 'full'
-  },
-  {
-    date: afterTomorrow,
-    status: 'partially'
-  }
-];
+// var tomorrow = new Date();
+// tomorrow.setDate(tomorrow.getDate() + 1);
+// var afterTomorrow = new Date();
+// afterTomorrow.setDate(tomorrow.getDate() + 1);
+// $scope.events = [
+//   {
+//     date: tomorrow,
+//     status: 'full'
+//   },
+//   {
+//     date: afterTomorrow,
+//     status: 'partially'
+//   }
+// ];
 
 function getDayClass(data) {
   var date = data.date,
@@ -209,5 +209,47 @@ function getDayClass(data) {
 
   return '';
 }
+
+
+//Create a Reservation Stuff
+_this.printBike = function(bikeID){
+  console.log(bikeID);
+  _this.dt.start.setHours(0,0,0,0);
+  _this.dt.end.setHours(0,0,0,0);
+  console.log("Dates:", _this.dt.start, _this.dt.end);
+}
+
+_this.transactionID = 0;
+_this.addReservation = function(){
+  _this.transactionID++
+  _this.dates = [];
+  _this.dt.start.setHours(0,0,0,0);
+  _this.dt.end.setHours(0,0,0,0);
+  while (_this.dt.start <= _this.dt.end){
+    _this.dates.push(new Date(_this.dt.start));
+    _this.dt.start.setTime(_this.dt.start.getTime() + 86400000);
+  }
+
+  _this.bikes = [];
+  _this.bikes.push(_this.selectedBike);
+  _this.reservationObj = {
+    "transactionID" : _this.transactionID,
+    "bikeID" : _this.bikes,
+    "resDate" : _this.dates,
+    "custName" : "Brian Schorn",
+    "custEmail" : "relcore@gamerarmy.com",
+    "custPhone" : "320-380-1060",
+    "custAddress" : "6200 Co Rd 120",
+    "pedalType" : "basic",
+    "needHelmet" : false,
+    "waiverSigned" : true,
+    "totalPrice" : 100
+  };
+  console.log(_this.reservationObj);
+  reservationService.addReservation(_this.reservationObj).then(function(){
+    console.log("reservation Added");
+  });
+
+};
 
 });
