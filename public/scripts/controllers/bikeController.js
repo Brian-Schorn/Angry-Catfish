0800ee1ebe20fb727ae485435407d22a811a2305
Erik Noonan <noonan135@gmail.com>
2017-03-06 16:51:26 -0600

    initial commit

--- public/scripts/controllers/bikeController.js
+++ public/scripts/controllers/bikeController.js
@@ -1,9 +1,10 @@
 angryCatfishApp.controller('bikeController', function ($http, $scope, $timeout, $interval, BikeService, ReservationService) {
   console.log('loaded Bike Controller');
+  var _this = this;
   var bikeService = BikeService;
   var reservationService = ReservationService;
 
-
+  _this.getBike
 
 
 
--- public/scripts/services/bikeService.js
+++ public/scripts/services/bikeService.js
@@ -3,23 +3,26 @@ angryCatfishApp.service('BikeService', function ($http) {
 
   // the public API
 
-    this.getBikes = function () {
+    this.getBike = function () {
       return $http.get('/bike');
   };
 
-  this.addBike = function (bike) {
-    $http.post('/bike', bike);
-    return $http.get('/bike');
-  };
+    //need function to add bike + date to reservation
 
-  this.updateBike = function (id, bike) {
-    $http.put('/bike/' + id, bike);
-    return $http.get('/bike');
-  };
-
-  this.deleteBike = function (id) {
-    $http.delete('/bike/' + id);
-    return $http.get('/bike');
-  };
+  //
+  // this.addBike = function (bike) {
+  //   $http.post('/bike', bike);
+  //   return $http.get('/bike');
+  // };
+  //
+  // this.updateBike = function (id, bike) {
+  //   $http.put('/bike/' + id, bike);
+  //   return $http.get('/bike');
+  // };
+  //
+  // this.deleteBike = function (id) {
+  //   $http.delete('/bike/' + id);
+  //   return $http.get('/bike');
+  // };
 
 });
