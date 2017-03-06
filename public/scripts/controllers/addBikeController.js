f8a6c441419186b0b4129a6f7f531f20ad1e5c5a
Brian Schorn <schorn.brian@gmail.com>
2017-03-06 16:19:12 -0600

    Completed Add Bike Form

--- public/scripts/controllers/addBikeController.js
+++ public/scripts/controllers/addBikeController.js
@@ -3,6 +3,12 @@ angryCatfishApp.controller('addBikeController', function ($http, $scope, $timeou
   var _this = this;
   var bikeService = BikeService;
   var reservationService = ReservationService;
+  _this.newBike = {};
+  _this.newBike.imageUrls = [];
+  _this.newBike.bulletPoints = [];
+  _this.newBike.searchTags = [];
+
+
 
 //RESET BUTTON FOR ADD BIKE FORM
   $scope.reset = function(form) {
@@ -14,6 +20,7 @@ angryCatfishApp.controller('addBikeController', function ($http, $scope, $timeou
   }
   $scope.reset();
 
+//SUBMIT BUTTON FOR ADD BIKE FORM
   _this.addBike = function(bike){
     console.log(bike);
     bikeService.addBike(bike).then(function(bikeList){
@@ -21,5 +28,30 @@ angryCatfishApp.controller('addBikeController', function ($http, $scope, $timeou
       console.log(_this.bikeList);
     });
   };
+
+//ADD IMAGE URL TO ARRAY
+  _this.addImage = function(imgURL){
+    console.log(imgURL);
+    _this.newBike.imageUrls.push(imgURL);
+    _this.newImage.newImage = null;
+
+  }
+
+//ADD BULLET POINT TO ARRAY
+_this.addBullet = function(bullet){
+  console.log(bullet);
+  _this.newBike.bulletPoints.push(bullet);
+  _this.newBullet= null;
+
+}
+
+//ADD BULLET POINT TO ARRAY
+_this.addTag = function(tag){
+  console.log(tag);
+  _this.newBike.searchTags.push(tag);
+  _this.newTag= null;
+
+}
+
 
 });
