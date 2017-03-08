20c39b6ae913589b001e859ba0f5f91300bc63f6
Brian Schorn <schorn.brian@gmail.com>
2017-03-08 17:26:03 -0600

    Added Date Filtering

--- public/scripts/controllers/authController.js
+++ public/scripts/controllers/authController.js
@@ -77,6 +77,7 @@ angryCatfishApp.controller('AuthController', function (AuthFactory, $http, $scop
     reservationService.getReservations().then(function(resList){
       _this.resList = resList.data;
       console.log("reservation list", _this.resList);
+      _this.checkDates();
     });
   };
   _this.getReservations();
@@ -183,6 +184,146 @@ angryCatfishApp.controller('AuthController', function (AuthFactory, $http, $scop
   $scope.handleDismiss = function (reason) {
     $log.info('Modal dismissed: ' + reason);
   }
+
+  //---------Calendar Stuff---------------
+  _this.today = function() {
+    console.log('today')
+    _this.dt = {};
+  _this.dt.start = new Date();
+  _this.dt.end = new Date();
+
+};
+_this.today();
+
+// _this.clear = function() {
+//   console.log('clear')
+//   _this.dt = null;
+// };
+
+// _this.submitDates = function(dt) {
+//   console.log('Dates:', dt)
+//   // check dates
+//   _this.clear();
+//   // .then(function(response){
+//   // }).catch(function(err){
+//   //   console.log('error checking dates', err)
+//   // });
+// };
+
+$scope.inlineOptions = {
+  customClass: getDayClass,
+  minDate: new Date(),
+  showWeeks: true
+};
+
+$scope.dateOptions = {
+  // dateDisabled: disabled,
+  formatYear: 'yy',
+  maxDate: new Date(2020, 5, 22),
+  minDate: new Date(),
+  startingDay: 1
+};
+
+// Disable weekend selection
+// function disabled(data) {
+//   var date = data.date,
+//     mode = data.mode;
+//   return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+// }
+
+// $scope.toggleMin = function() {
+//   $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+//   $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+// };
+//
+// $scope.toggleMin();
+
+$scope.open1 = function() {
+  $scope.popup1.opened = true;
+};
+
+$scope.open2 = function() {
+  $scope.popup2.opened = true;
+};
+
+
+
+$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+$scope.format = $scope.formats[0];
+$scope.altInputFormats = ['M!/d!/yyyy'];
+
+$scope.popup1 = {
+  opened: false
+};
+
+$scope.popup2 = {
+  opened: false
+};
+
+// var tomorrow = new Date();
+// tomorrow.setDate(tomorrow.getDate() + 1);
+// var afterTomorrow = new Date();
+// afterTomorrow.setDate(tomorrow.getDate() + 1);
+// $scope.events = [
+//   {
+//     date: tomorrow,
+//     status: 'full'
+//   },
+//   {
+//     date: afterTomorrow,
+//     status: 'partially'
+//   }
+// ];
+
+    function getDayClass(data) {
+      var date = data.date,
+        mode = data.mode;
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+        for (var i = 0; i < $scope.events.length; i++) {
+          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
+          }
+        }
+      }
+
+      return '';
+    }
+
+  //Calender Filtering Stuff
+  // _this.availability = ["58bf0cff7970320d37d3e2b1"];
+  _this.availability = [];
+
+  _this.checkDates = function() {
+    _this.availability = [];
+    _this.dt.start.setHours(0,0,0,0);
+    _this.dt.end.setHours(23,59,59,999);
+    _this.filter = {};
+    _this.filter.start = _this.dt.start.getTime();
+    _this.filter.end = _this.dt.end.getTime();
+
+    console.log(_this.filter.start);
+    console.log(_this.filter.end);
+    console.log("Reservation List",_this.resList);
+    _this.resList.forEach(function(res){
+      _this.filter.resBikeId = res.bikeID[0];
+      console.log("BikeId",_this.filter.resBikeId);
+      res.resDate.forEach(function(resDate){
+        _this.filter.query = new Date(resDate).getTime();
+        console.log("resDate",_this.filter.query);
+        if((_this.filter.query >= _this.filter.start) && (_this.filter.query < _this.filter.end)){
+          console.log("Conflict!", _this.filter.resBikeId);
+          _this.availability.push(_this.filter.resBikeId);
+        }
+      })
+    })
+    console.log("Availability Conflicts", _this.availability);
+  }
+
+
 
 
 
