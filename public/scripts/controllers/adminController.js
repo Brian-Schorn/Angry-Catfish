27455ca56059eaf9c6fdc7c7c722106a3e548fb4
Brian Schorn <schorn.brian@gmail.com>
2017-03-13 11:07:01 -0500

    Moved Google Auth to /admin page

--- public/scripts/controllers/adminController.js
+++ public/scripts/controllers/adminController.js
@@ -1,6 +1,10 @@
-angryCatfishApp.controller('adminController', function ($http, $scope, $timeout, $interval, $uibModalStack, BikeService, ReservationService) {
+angryCatfishApp.controller('adminController', function (AuthFactory, $http, $scope, $timeout, $interval, $uibModalStack, BikeService, ReservationService) {
   console.log('loaded Admin Controller');
   var _this = this;
+
+  var authFactory = AuthFactory;
+  _this.loggedIn = authFactory.checkLoggedIn();
+  console.log(authFactory.checkLoggedIn());
   var bikeService = BikeService;
   var reservationService = ReservationService;
 
