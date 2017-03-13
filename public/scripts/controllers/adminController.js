ef6f0c27d01e1fb0247f57becaaae852c8374c1f
Brian Schorn <schorn.brian@gmail.com>
2017-03-13 11:01:47 -0500

    Added Admin Page (Is Currently Blank)

---
+++ public/scripts/controllers/adminController.js
@@ -0,0 +1,9 @@
+angryCatfishApp.controller('adminController', function ($http, $scope, $timeout, $interval, $uibModalStack, BikeService, ReservationService) {
+  console.log('loaded Admin Controller');
+  var _this = this;
+  var bikeService = BikeService;
+  var reservationService = ReservationService;
+
+
+
+});
