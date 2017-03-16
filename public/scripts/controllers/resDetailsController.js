1a85a35a46abd3361157dadb57c9de8c4f39abdb
Tyler Herman <tylerherman@Tylers-MacBook-Pro.local>
2017-03-16 14:02:46 -0500

    updated res details

--- public/scripts/controllers/resDetailsController.js
+++ public/scripts/controllers/resDetailsController.js
@@ -14,6 +14,7 @@ angryCatfishApp.controller('resDetailsController', function ($http, $scope, $tim
     reservationService.getReservationsByTransactionID(_this.modalID).then(function(resList){
       _this.resData = resList.data[0];
       console.log("Reservation Info for this TransactionID", _this.resData);
+      _this.getBikes();
     });
   };
   _this.getReservations();
@@ -21,24 +22,25 @@ angryCatfishApp.controller('resDetailsController', function ($http, $scope, $tim
 
   //Grabs all the bikes from the DB
   _this.getBikes = function(){
+    _this.bikeNames = []
     bikeService.getBikes().then(function(bikeList){
       _this.bikeList = bikeList.data;
       console.log('bike list', _this.bikeList);
-      //Pulls bike ID from params
-      _this.bikeID = _this.modalID;
-      console.log(_this.bikeID);
-
       //Finds matching Bike in bikeList
-      _this.bikeList.forEach(function(bike){
-        if (bike._id == _this.bikeID){
-          _this.selectedBike = bike;
-          console.log(_this.selectedBike);
-        }
+      _this.resData.bikeID.forEach(function(bikeID){
+        console.log("bikeID",bikeID);
+        _this.bikeList.forEach(function(matchID){
+          console.log("matchID",matchID);
+          if (bikeID == matchID._id){
+            _this.bikeNames.push(matchID.bikeMake + " " + matchID.bikeModel)
+          }
+          console.log(_this.bikeNames);
+        })
       })
     });
   };
 
-  _this.getBikes();
+
 
 
 });
